8c53a47ac54c755bb08bb0c1d1a190209d41260b
eliot_simulator <eliot.mp99@gmail.com>
2024-04-16 11:02:56 +0200

    start eliot branch

--- lab1/gale_shapley.py
+++ lab1/gale_shapley.py
@@ -1,8 +1,39 @@
 import sys
 
+'''
+Gale-Shapley algorithm solving the stable matching problem
+a_pref - proposing entities preferences list ("students"), 
+b_pref - receiving entities preferences list ("companies")
+'''
+def gale_shapley(b_pref, a_pref):
+    b_matches = {}
+    a_matches = {}
 
-def gs(company_prefs, student_prefs):
-    return 0
+    # while there are unassigned b's
+    while len(b_pref) > 0:
+        b, preferences = b_pref.popitem()
+        a = preferences.pop(0)
+
+        # if a is not matched, match a and b
+        if a not in a_matches:
+            a_matches[a] = b
+            b_matches[b] = a
+        # if a is matched, check if a prefers b over current match
+        else:
+            current_b = a_matches[a]
+            current_b_preferences = a_pref[current_b]
+            # if a prefers b over current match, match a and b and unmatch current match
+            if current_b_preferences.index(b) < current_b_preferences.index(current_b):
+                b_pref[b] = preferences
+            # if a does not prefer b over current match, add a back to b's preferences
+            else:
+                b_pref[current_b] = current_b_preferences[1:]
+                b_matches[b] = a
+                a_matches[a] = b
+
+    # return the stable matches
+    return b_matches
+    
 
 
 def line_to_dict(line, c_prefs, s_prefs):
@@ -11,10 +42,11 @@ def line_to_dict(line, c_prefs, s_prefs):
     key = ints[0]
     values = ints[1:]
 
-    if not key in c_prefs: #första förekomsten är company
+    if not key in c_prefs: #första förekomsten är b
         c_prefs[key] = values
-    else: #andra förekomsten är student
+    else: #andra förekomsten är a
         s_prefs[key] = values
+
 
 def parse():
     c_prefs = {}
@@ -29,7 +61,7 @@ def parse():
     return c_prefs, s_prefs, N
 
 
-#Ska användas för att sortera company prefs enligt hint på slide 26 F01
+#Ska användas för att sortera b prefs enligt hint på slide 26 F01
 #funkar bara utan repetitioner och om samtliga tal från min till max förekommer
 def pref_to_index(prefs): 
     
@@ -41,19 +73,17 @@ def pref_to_index(prefs):
 
 
 def main():
-    #parsea fil, dela upp i student och company prefs (dictionaries {s:[c,...],s...}, {c:[s,...],c...})
+    #parsea fil, dela upp i a och b prefs (dictionaries {s:[c,...],s...}, {c:[s,...],c...})
     c_prefs, s_prefs, N = parse()
     print(c_prefs)
     print(s_prefs)
     print(N)
 
-
     prefs = pref_to_index([4,2,1,3])
     print("PREFS", prefs)
 
-    result = gs(c_prefs, s_prefs)
-    #print(result)
-
+    result = gale_shapley(c_prefs, s_prefs)
+    print("GS Result: ", result)
 
 if __name__ == "__main__":
     main()
