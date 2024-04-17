60098c30f092468e4aaa8b10e2bc6d675141af29
eliot_simulator <eliot.mp99@gmail.com>
2024-04-17 08:56:42 +0200

    stuff

--- lab1/gale_shapley.py
+++ lab1/gale_shapley.py
@@ -1,49 +1,68 @@
 import sys
 
-'''
-Gale-Shapley algorithm solving the stable matching problem
-a_pref - proposing entities preferences list ("students"), 
-b_pref - receiving entities preferences list ("companies")
-'''
-def gale_shapley(a_pref, b_pref):
-    b_matches = {}
-    a_matches = {}
-    a_free = list(a_pref.keys())
-    # While there are free a's
-    while a_free:
-        a = a_free.pop(0)
-        b = a_pref[a].pop(0)
-        # If b is free, match a and b
-        if b in b_matches:
-            a2 = b_matches[b]
-            # If a prefers b over a2, match a and b and free a2
-            if b_pref[b].index(a) < b_pref[b].index(a2):
-                a_matches[a] = b
-                a_free.append(a2)
-            else:
-                a_free.append(a)
+def gs(c_prefs, s_prefs):
+
+    p = list(s_prefs.keys())
+    matches = {}
+    applied = {student: [] for student in s_prefs}
+    
+    while p:
+        # Take the first student from p
+        s = p.pop(0)
+        # Find the next preferred company that hasn't been applied to yet
+        for c in s_prefs[s]:
+            if c not in applied[s]:
+                applied[s].append(c)
+                break
         else:
-            b_matches[b] = a
-            a_matches[a] = b
-    return a_matches
+            # If all companies have been applied to, skip further processing
+            continue
+
+        if c not in matches: #AAAAAAAAAAAAA
+            # If the company has no student, match it with s
+            matches[c] = s
+
+        else:
+            # If the company already has a student, check the company's preference
+            current_student = matches[c]
+            # Check if s is in the company's preference list and compare preferences
+            if s in c_prefs[c] and (c_prefs[c].index(s) < c_prefs[c].index(current_student)):
+                # If the company prefers s over its current student
+                matches[c] = s
+                # Add the displaced student back to p
+                if current_student not in p:
+                    p.append(current_student)
+            elif current_student not in p:
+                # If s is not in the preference list, or if the current student is preferred,
+                # re-add the current student to p to reconsider their options
+                p.append(s)
+
+    return matches
 
 
 def line_to_dict(line, c_prefs, s_prefs):
     parts = line.strip().split()
     ints = [int(x) for x in parts]
     key = ints[0]
-    values = ints[1:]
+    values = []
+    if len(ints) > 1: #skippa tomma preflistor
+        values = ints[1:]
+    else:
+       return
 
-    if not key in c_prefs: #första förekomsten är b
+    if not key in c_prefs: #första förekomsten är company
         c_prefs[key] = values
-    else: #andra förekomsten är a
+    elif not key in s_prefs: #andra förekomsten är student
         s_prefs[key] = values
-
+    elif key in c_prefs and key in s_prefs: #tredje förekomsten ska inte ske, skippa
+        return
+        
 
 def parse():
     c_prefs = {}
     s_prefs = {}
-    N = int(sys.stdin.readline())
+    first_line = sys.stdin.readline()
+    N = int(first_line)
 
     for line in sys.stdin:
         line_to_dict(line, c_prefs, s_prefs)
@@ -53,7 +72,7 @@ def parse():
     return c_prefs, s_prefs, N
 
 
-#Ska användas för att sortera b prefs enligt hint på slide 26 F01
+#Ska användas för att sortera company prefs enligt hint på slide 26 F01
 #funkar bara utan repetitioner och om samtliga tal från min till max förekommer
 def pref_to_index(prefs): 
     
@@ -66,7 +85,6 @@ def pref_to_index(prefs):
 #resultat från gs-algoritmen, dictionary med företag (nyckel) - studentpar.
 def output(r_dic):
     r_sorted = dict(sorted(r_dic.items()))
-
     for c in r_sorted:
         print(r_sorted[c])
 
@@ -75,7 +93,11 @@ def output(r_dic):
 def main():
     #parsea fil, dela upp i student och company prefs (dictionaries {s:[c,...],s...}, {c:[s,...],c...})
     c_prefs, s_prefs, N = parse()
-    result = gale_shapley(s_prefs, c_prefs)
+ 
+    #prefs = pref_to_index([4,2,1,3])
+    #print("PREFS", prefs)
+
+    result = gs(c_prefs, s_prefs)
     output(result)
 
 
