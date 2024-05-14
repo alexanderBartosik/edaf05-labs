31b6da55e6a4766d190786738eefcb00e7379329
eliot_simulator <eliot.mp99@gmail.com>
2024-05-14 12:56:11 +0200

    compact parse

--- lab4/eliotSol.py
+++ lab4/eliotSol.py
@@ -12,31 +12,23 @@ methods and sorting.
 
 # Parses the content of lab 4 input files
 def parse():
-    
-    # Extract the first line of the input file (N: num players)
-    first_line = sys.stdin.readline().strip().split()
-    N = int(first_line[0]) # only 1 number in lab 4 input files
-
-    # Extract the rest of the lines (x: x-coord, y: y-coord)
-    all_lines_of_ints = []
-    for line in sys.stdin:
-        line_of_ints = [int(x) for x in line.strip().split()]
-        all_lines_of_ints.append(line_of_ints)
-    x = [line[0] for line in all_lines_of_ints]
-    y = [line[1] for line in all_lines_of_ints]
-
-    return N, x, y
+    N, *points = [tuple(map(int, line.strip().split())) for line in sys.stdin]
+    return N, points
 
 '''
-The algo function should return the answer to the problem
+Divide and conquer algorithm that finds the closest pair of points in a set of points.
+
+Time complexity: O(n log n) according to the Master Theorem
+
+points - a list of tuples (x, y) representing the coordinates of the points
 '''
-def algo(x, y):
+def find_closest_pair(points):
 
     def distance(p1, p2):
         return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) # euclidean norm 2 coordinates
     
     # closest_cross_pair is a helper function that finds the closest pair of points
-    def closest_cross_pair(points, strip, delta):
+    def closest_cross_pair(strip, delta):
         strip.sort(key=lambda point: point[1])  # Sort strip by y-coordinate
         min_dist = delta
         for i in range(len(strip)):
@@ -46,32 +38,35 @@ def algo(x, y):
                     min_dist = dist
         return min_dist
 
-    # clostest_recursive is a recursive function that finds the closest pair of points    
+    '''
+    closest_recursive is a recursive function that finds the closest pair of points
+    in a set of points.
+    Divide into two sets, find the closest pair in each set, and then find the closest pair
+    '''
     def closest_recursive(points):
-        # If there are only 3 or fewer points, calculate and return the closest pair
+        # BOTTOM CASE: If there are only 3 or fewer points, calculate and return the closest pair
         if len(points) <= 3:
             return min(distance(points[i], points[j]) for i in range(len(points)) for j in range(i + 1, len(points)))
 
-        # Find the middle point
+        # Divide the points into two sets (left/right)
         mid = len(points) // 2
         mid_point = points[mid]
         left = points[:mid]
         right = points[mid:]
 
-        # Subdivide the points into two sets and find the closest pair in each set
+        # Recursively find the closest pair of points in each set
         dl = closest_recursive(left)
         dr = closest_recursive(right)
-        d = min(dl, dr)
+        d = min(dl, dr) # Find the minimum distance between the two sets
 
         # Find the closest pair of points that are in different sets
         strip = [p for p in points if abs(p[0] - mid_point[0]) < d]
-        return min(d, closest_cross_pair(points, strip, d))
+        return min(d, closest_cross_pair(strip, d))
 
     # Converting coordinates to point list and initiating the process
-    points = list(zip(x, y))
-    points.sort(key=lambda point: point[0])  # Sort points by x-coordinate
+    points.sort(key=lambda point: point[0])  # Sort points by x-coordinate O(n log n)
     closest_distance = closest_recursive(points)
-    return format(closest_distance, '.6f')
+    return format(closest_distance, '.6f') # Return on desired format
 
 def write_to_output_file(start, stop):
     runtime = stop - start
@@ -79,15 +74,15 @@ def write_to_output_file(start, stop):
         file.write("Runtime: " + str(runtime) + '\n')
 
 def main():
-    _, x, y = parse()
+    _, points = parse()
 
     start = time.time()
-    answer = algo(x, y)
+    closest_dist = find_closest_pair(points)
     stop = time.time()
 
     write_to_output_file(start, stop)
 
-    print(answer)
+    print(closest_dist)
 
 if __name__ == "__main__":
     main()
