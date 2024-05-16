823cb05f30938c2ef4d1f67165c15c9b18ec08d9
Alexander Bartosik <alex.bartosik@gmail.com>
2024-05-16 13:47:19 +0200

    lab 5 comments

--- lab5/seq_alignment.py
+++ lab5/seq_alignment.py
@@ -28,7 +28,7 @@ def needleman_wunsch(seq1, seq2, letters, score_matrix, gap_penalty=-4):
     scores = [[0] * (m + 1) for _ in range(n + 1)]
     traceback = [[None] * (m + 1) for _ in range(n + 1)]
 
-    # Initialize the first row and column
+    #initialize the first row and column. "BASE CASE" (scores for aligning with gap in beginning)
     for i in range(1, n + 1):
         scores[i][0] = i * gap_penalty
         traceback[i][0] = 'UP'
@@ -36,7 +36,8 @@ def needleman_wunsch(seq1, seq2, letters, score_matrix, gap_penalty=-4):
         scores[0][j] = j * gap_penalty
         traceback[0][j] = 'LEFT'
 
-    # Fill the scoring matrix and update the traceback matrix
+    #fill scoring matrix and update traceback matrix
+    #O(n * m), everything inside is O(1)
     for i in range(1, n + 1):
         for j in range(1, m + 1):
             char1 = seq1[i - 1]
@@ -48,9 +49,9 @@ def needleman_wunsch(seq1, seq2, letters, score_matrix, gap_penalty=-4):
             else:
                 match_score = gap_penalty
             
-            match = scores[i - 1][j - 1] + match_score
-            delete = scores[i - 1][j] + gap_penalty
-            insert = scores[i][j - 1] + gap_penalty
+            match = scores[i - 1][j - 1] + match_score #match = diag down ight
+            delete = scores[i - 1][j] + gap_penalty #delete = down (append seq1 with *)
+            insert = scores[i][j - 1] + gap_penalty #insert = right (append seq2 with *)
             
             # Choose the highest scoring option
             best_score = max(match, delete, insert)
@@ -68,7 +69,7 @@ def needleman_wunsch(seq1, seq2, letters, score_matrix, gap_penalty=-4):
     aligned_seq2 = []
     i, j = n, m
     
-    while i > 0 or j > 0:
+    while i > 0 or j > 0: #worst case O(n+m)?
         if traceback[i][j] == 'DIAG':
             aligned_seq1.append(seq1[i - 1])
             aligned_seq2.append(seq2[j - 1])
